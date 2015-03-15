dfe75876eb9c12bdff730b75a32fcaf7af99f129
Alec Rust <me@alecrust.com>
2015-03-15 15:04:02 +0000

    Add support for WordPress 4.1 title-tag improvements
    https://make.wordpress.org/core/2014/10/29/title-tags-in-4-1/
    
    closes #1

--- src/header.php
+++ src/header.php
@@ -10,7 +10,6 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <title><?php wp_title( '|', true, 'right' ); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="msapplication-config" content="<?php echo get_template_directory_uri() . '/browserconfig.xml'; ?>">
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri() . '/favicon.ico'; ?>">
--- src/inc/extras.php
+++ src/inc/extras.php
@@ -31,37 +31,7 @@ function wpg_slug_body_class( $classes ) {
 }
 add_filter( 'body_class', 'wpg_slug_body_class' );
 
-/**
- * Filters wp_title to print a neat <title> tag based on what is being viewed.
- *
- * @param string $title Default title text for current view.
- * @param string $sep Optional separator.
- * @return string The filtered title.
- */
-function wpg_wp_title( $title, $sep ) {
-  if ( is_feed() ) {
-    return $title;
-  }
 
-  global $page, $paged;
-
-  // Add the site name
-  $title .= get_bloginfo( 'name', 'display' );
-
-  // Add the site description for the front page
-  $site_description = get_bloginfo( 'description', 'display' );
-  if ( is_front_page() ) {
-    $title .= " $sep $site_description";
-  }
-
-  // Add a page number if necessary
-  if ( ( $paged >= 2 || $page >= 2 ) && ! is_404() ) {
-    $title .= " $sep " . sprintf( __( 'Page %s' ), max( $paged, $page ) );
-  }
-
-  return $title;
-}
-add_filter( 'wp_title', 'wpg_wp_title', 10, 2 );
 
 /**
  * Adds .Button classes to Edit link
