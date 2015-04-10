77304c76e75cc58abf02e658ae52e4f57ea40abb
Alec Rust <me@alecrust.com>
2015-04-10 11:15:43 +0100

    Remove unnecessary postcss-import
    - Revert syntax in gulp/tasks/styles.js

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,7 +4,6 @@ var stylus =      require('gulp-stylus');
 var postcss =     require('gulp-postcss');
 var clip =        require('gulp-clip-empty-files');
 var bemLinter =   require('postcss-bem-linter');
-var atImport =    require('postcss-import');
 var cssnext =     require('cssnext');
 var del =         require('del');
 var notifyError = require('../notifyError');
@@ -23,29 +22,26 @@ gulp.task('stylus', function() {
  * Lint all built CSS files individually
  */
 gulp.task('bemlint', ['stylus'], function() {
-  var processors = [
-    bemLinter
-  ];
   return gulp.src(path.join(paths.styles.tmpDir, '**/*.css'))
     .pipe(clip())
-    .pipe(postcss(processors).on('error', notifyError));
+    .pipe(postcss([
+      bemLinter(),
+    ]).on('error', notifyError));
 });
 
 /**
  * Process CSS files with PostCSS and generate built files
  */
 gulp.task('postcss', ['stylus', 'bemlint'], function() {
-  var processors = [
-    atImport,
-    cssnext({
+  return gulp.src(path.join(paths.styles.tmpDir, '*.css'))
+    .pipe(postcss([
+      cssnext({
       url: false,
       features: {
         rem: false
       }
     })
-  ];
-  return gulp.src(path.join(paths.styles.tmpDir, '*.css'))
-    .pipe(postcss(processors).on('error', notifyError))
+    ]).on('error', notifyError))
     .pipe(gulp.dest(paths.styles.dest));
 });
 
