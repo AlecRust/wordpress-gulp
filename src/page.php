959e57cd41754754dbae067c0ca6757b25bc9bf7
Alec Rust <me@alecrust.com>
2015-08-09 16:16:56 +0100

    Bump package.json dependencies
    - Tidy up a couple PHP comments

--- src/page.php
+++ src/page.php
@@ -4,7 +4,7 @@
  *
  * This is the template that displays all pages by default.
  * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
+ * and that other 'pages' on your WordPress site may use a
  * different template.
  *
  * @package wordpress-gulp
