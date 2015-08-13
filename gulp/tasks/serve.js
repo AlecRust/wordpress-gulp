394434b38620047cef360b3a5e4f77b368fc2ce7
Alec Rust <me@alecrust.com>
2015-08-13 20:25:36 +0100

    Update local dev URL

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -2,5 +2,5 @@ var gulp = require('gulp');
 var opn =  require('opn');
 
 gulp.task('serve', function () {
-  opn('http://localhost:8888/wordpress-gulp/');
+  opn('http://local.dev:8888/wordpress-gulp/');
 });
