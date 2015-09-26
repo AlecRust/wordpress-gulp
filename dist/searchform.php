4b637cf52686615f7520a06e5d3c0c975b527e35
Alec Rust <me@alecrust.com>
2015-09-26 18:10:14 +0100

    Remove unprintable character from searchform.php

--- dist/searchform.php
+++ dist/searchform.php
@@ -9,7 +9,7 @@
 <form method="get" action="<?php echo home_url( '/' ); ?>" role="search">
   <label>
     <span class="u-hiddenVisually">Search for:</span>
-    <input class="InputText InputText--sm" type="search" placeholder="Search…" value="" name="s">
+    <input class="InputText InputText--sm" type="search" placeholder="Search..." value="" name="s">
   </label>
   <button class="Button Button--sm Button--default" type="submit">Search</button>
 </form>
