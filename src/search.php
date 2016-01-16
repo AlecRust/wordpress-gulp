c06abc17ea6dd843f05e0a33399bc77856f58a53
Alec Rust <me@alecrust.com>
2016-01-16 22:29:09 +0000

    Improve formatting of conditionals in search template

--- src/search.php
+++ src/search.php
@@ -15,26 +15,18 @@ get_header(); ?>
       <h1 class="PageHeader-title"><?php printf( esc_html__( 'Search Results for: %s', 'wpg' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
     </header>
 
-    <?php while ( have_posts() ) : the_post(); ?>
-
-      <?php
-      /**
-       * Run the loop for the search to output the results.
-       * If you want to overload this in a child theme then include a file
-       * called content-search.php and that will be used instead.
-       */
+    <?php while ( have_posts() ) : the_post();
       get_template_part( 'template-parts/content', 'search' );
-      ?>
 
-    <?php endwhile; ?>
+    endwhile;
 
-    <?php the_posts_navigation(); ?>
+    the_posts_navigation();
 
-  <?php else : ?>
+  else :
 
-    <?php get_template_part( 'template-parts/content', 'none' ); ?>
+    get_template_part( 'template-parts/content', 'none' );
 
-  <?php endif; ?>
+  endif; ?>
 
   </main>
 
