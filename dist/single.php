a0cdee4abc0110609c357b46c2f117746a3e98c4
Alec Rust <me@alecrust.com>
2016-06-23 21:28:38 +0100

    Update further templates with latest from underscores

--- dist/single.php
+++ dist/single.php
@@ -11,22 +11,18 @@ get_header(); ?>
 
     <main class="Container-mainColumn" role="main">
 
-      <?php while ( have_posts() ) : the_post(); ?>
+      <?php while ( have_posts() ) : the_post();
 
-        <?php get_template_part( 'template-parts/content', 'single' ); ?>
+        get_template_part( 'template-parts/content', get_post_format() );
 
-        <?php the_post_navigation(); ?>
+        the_post_navigation();
 
-        <?php
-          // If comments are open or we have at least one comment, load up the comment template
-          if ( comments_open() || get_comments_number() ) :
-            comments_template();
-          endif;
-        ?>
+        // If comments are open or we have at least one comment, load up the comment template.
+        if ( comments_open() || get_comments_number() ) :
+          comments_template();
+        endif;
 
-      <?php endwhile; ?>
-
-    </main>
+      endwhile; ?>
 
     <?php get_sidebar(); ?>
 
--- src/page.php
+++ src/page.php
@@ -14,18 +14,16 @@ get_header(); ?>
 
   <main class="Container" role="main">
 
-    <?php while ( have_posts() ) : the_post(); ?>
+    <?php while ( have_posts() ) : the_post();
 
-      <?php get_template_part( 'template-parts/content', 'page' ); ?>
+      get_template_part( 'template-parts/content', 'page' );
 
-      <?php
-        // If comments are open or we have at least one comment, load up the comment template
-        if ( comments_open() || get_comments_number() ) :
-          comments_template();
-        endif;
-      ?>
+      // If comments are open or we have at least one comment, load up the comment template.
+      if ( comments_open() || get_comments_number() ) :
+        comments_template();
+      endif;
 
-    <?php endwhile; ?>
+    endwhile; ?>
 
   </main>
 
