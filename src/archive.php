9edf77d1ee4e1b0bcdcd6ab53c9b8759d0767ad9
Alec Rust <me@alecrust.com>
2016-06-23 21:07:02 +0100

    Update `archive.php` with latest underscores changes

--- src/archive.php
+++ src/archive.php
@@ -20,26 +20,24 @@ get_header(); ?>
           ?>
         </header>
 
-        <?php while ( have_posts() ) : the_post(); ?>
+        <?php while ( have_posts() ) : the_post();
 
-          <?php
-            /*
-             * Include the Post-Format-specific template for the content.
-             * If you want to override this in a child theme, then include a file
-             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-             */
-            get_template_part( 'template-parts/content', get_post_format() );
-          ?>
+          /*
+           * Include the Post-Format-specific template for the content.
+           * If you want to override this in a child theme, then include a file
+           * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+           */
+          get_template_part( 'template-parts/content', get_post_format() );
 
-        <?php endwhile; ?>
+        endwhile;
 
-        <?php the_posts_navigation(); ?>
+        the_posts_navigation();
 
-      <?php else : ?>
+      else :
 
-        <?php get_template_part( 'template-parts/content', 'none' ); ?>
+        get_template_part( 'template-parts/content', 'none' );
 
-      <?php endif; ?>
+      endif; ?>
 
     </main>
 
