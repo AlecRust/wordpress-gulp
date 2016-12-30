3ecd250b1fbc40bd6304e1c3c57dfe9f211a2691
Alec Rust <me@alecrust.com>
2016-12-30 17:15:42 +0000

    Fix `gulp watch` only picking up two main CSS files

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,7 +2,7 @@ var gulp  = require('gulp');
 var paths = require('../paths');
 
 gulp.task('watch', ['build'], function() {
-  gulp.watch(paths.styles.cssSrc, ['styles']);
+  gulp.watch('src/assets/styles/**/*.css', ['styles']);
   gulp.watch(paths.scripts.jsSrc, ['scripts']);
   gulp.watch(paths.images.imgSrc, ['images']);
   gulp.watch('src/**/*.php', ['copy']);
