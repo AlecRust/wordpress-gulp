de14a3bd5c7f388e5b7ddcae0b5926fb88ad4036
Alec Rust <me@alecrust.com>
2017-01-24 20:40:46 +0000

    Add gulp `release` task

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,12 @@ var minifyCss = require('gulp-cssnano');
 var del = require('del');
 var cache = require('gulp-cache');
 var imagemin = require('gulp-imagemin');
+var bump = require('gulp-bump');
+var touch = require('gulp-touch');
 var concat = require('gulp-concat');
+var git = require('gulp-git');
+var fs = require('fs');
+var clip = require('gulp-clip-empty-files');
 var postcss = require('gulp-postcss');
 var atImport = require('postcss-import');
 var at2x = require('postcss-at2x');
@@ -36,6 +41,12 @@ var paths = {
 
 function clean() {
   return del([ paths.dist ]);
+}
+
+function getPackageJsonVersion() {
+  // Parse the JSON file instead of using require because require
+  // caches multiple calls so the version number won't be updated
+  return JSON.parse(fs.readFileSync('./package.json', 'utf8')).version;
 }
 
 function styles() {
@@ -93,6 +104,36 @@ function watch() {
   gulp.watch('src/**/*.php', copy);
 }
 
+function bumpVersion() {
+  return gulp.src([
+    'package.json',
+    'src/assets/styles/style.styl',
+    'src/functions.php',
+    'src/readme.txt',
+  ], { base: './' })
+  .pipe(bump())
+  .pipe(gulp.dest('./'))
+  // Touch the files to ensure changes are
+  // picked up by Git: https://git.io/vMNKZ
+  .pipe(touch());
+}
+
+function commitChanges() {
+  var version = getPackageJsonVersion();
+  return gulp.src('.')
+    .pipe(git.add())
+    .pipe(git.commit('Release version ' + version));
+}
+
+function createNewTag(cb) {
+  var version = getPackageJsonVersion();
+  return git.tag(version, 'Created Tag for version: ' + version, function (error) {
+    if (error) {
+      return cb(error);
+    }
+  });
+}
+
 exports.clean = clean;
 exports.styles = styles;
 exports.scripts = scripts;
@@ -101,5 +142,6 @@ exports.images = images;
 var build = gulp.series(clean, gulp.parallel(styles, scripts, images), copy);
 
 gulp.task('watch', gulp.series(build, watch));
+gulp.task('release', gulp.series(bumpVersion, build, commitChanges, createNewTag));
 gulp.task('build', build);
 gulp.task('default', build);
